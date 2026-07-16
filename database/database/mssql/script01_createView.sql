886f6dd3e93937e12dc611dffdda39f8bfcf0144
Max Balin <envionx@gmail.com>
2026-07-15 22:27:16 -0400

    Improve random result

--- database/database/mssql/script01_createView.sql
+++ database/database/mssql/script01_createView.sql
@@ -1152,6 +1152,7 @@ AS
         WHERE EXISTS (SELECT 1 FROM dbo.lake_fish f WHERE f.lake_Id = w.lakeId)
         AND w.supported = 1 
         AND EXISTS (SELECT 1 FROM dbo.ows_meteo o WHERE o.mli=w.mli AND CAST(o.stamp AS DATE) < CAST(GETDATE() AS DATE))
+        ORDER BY NEWID() OFFSET 0 ROWS
         UNION ALL
         SELECT mli, lat, lon, country, state, sid, stamp 
         FROM dbo.WaterStation w 
@@ -1159,8 +1160,7 @@ AS
         AND w.supported = 0
         AND EXISTS (SELECT 1 FROM dbo.ows_meteo o WHERE o.mli=w.mli AND CAST(o.stamp AS DATE) < CAST(GETDATE() AS DATE))
     ) AS UnionedData
-    ORDER BY NEWID()
-    OFFSET 0 ROWS;
+
 GO
 -------------------------------------------------------------------------------------------------------
 
